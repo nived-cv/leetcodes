5b97777bcce55f4dd19025e9763f1b70fcc9f579
nived-cv <nived.cv@vonnue.com>
2024-05-06 12:46:59 +0530

    fix: bad

--- largestPositiveWithItsNegative.js
+++ largestPositiveWithItsNegative.js
@@ -2,23 +2,28 @@
 const main = (nums) => {
     const hashRes = {}
 
-    nums.forEach((num) => {
+    for(let num of nums){
+
         if(num in hashRes){
             hashRes[num] = -num
         } else {
-            hashRes[-num] = 0
-        }
-    })
-
-    for (let key of hashRes){
-        if(key in hashRes){
-            
+            hashRes[-num] = hashRes[-num] ?? 0
         }
     }
 
-    return -1
+    let largest = -1
+    for (let key in hashRes){
+        if(hashRes[key] === 0){
+            continue
+        }
+        let temp = +key
+        temp = +key > 0 ? +key : +-key
+        largest = temp > largest ? temp : largest
+    }
+
+    return largest
 }
 
-let raw = [-1,10,6,7,-7,1]
+let raw = [-11,28,23,41,33,-14,-32,-35,34,-31,28,-26,38,4,16,33,-48,28,-13,-35,-7,-49,3,7,43,18,20,-37,-27,-38,-30,-37,49,-33,33,-14,-49,-38,-25,3,31]
 
 console.log(main(raw))
