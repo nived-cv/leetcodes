6ac2306b0fa37cd44703ad3be239c25afb1a425f
nived-cv <nived.cv@vonnue.com>
2024-05-29 08:40:29 +0530

    chore(stats): commiting to get lines of count

--- wordBreak.js
+++ wordBreak.js
@@ -1,46 +1,11 @@
 /**
- * @param {string} currentIndex
- * @param {string[]} chain
- * @param {Object} dict
- */
-
-const returnIndexChain = (currentIndex, chain, dict) => {
-        let index = currentIndex
-        const result = chain
-        while(String(index) in dict){
-            result.push(index)
-            index = dict[index][0]
-            console.log('index', index, dict)
-        }
-
-    return result
-}
-
-/**
  * @param {string} string
  * @param {string[]} dict
  * @return {string[]}
  */
 const wordBreak = (string, dict) => {
     const wordMap = {}
-
-   for(let word of dict){
-    const index = string.indexOf(word)
-
-    if(index > -1){
-        if(index in wordMap){
-            wordMap[index].push(index + word.length)
-            continue
-        }
-        wordMap[index] = [index + word.length]
-    }
-   }
-
-   
-   const chain = returnIndexChain(0, [], wordMap)
-
-    return chain
-
+    
 }
 
 const testCases = [
@@ -60,38 +25,3 @@ const test = (testFn, testCases) => {
 
 // test(wordBreak, testCases)
 console.log(wordBreak(...testCases[0]))
-
-
-
-
-
-
-
-// solution 1
-// const connectSentence = (dict, string) => {
-
-//     for (let word of dict){
-//         const index = string.indexOf(word)
-//         if(index === 0){
-//             const remaining = string.slice(word.length)
-//             return [word , remaining ]
-//         }
-//     }
-//     return ['', '']
-// }
-
-// const wordBreak = (string, dict) => {
-    
-//     let remaining = string
-//     let sentence = ''
-
-//     while(remaining.length > 0){
-//         [word, remaining] =  connectSentence(dict, remaining)
-//         if(!word){
-//             return []
-//         }
-//         sentence = sentence + word + ' '
-//     }
-    
-//     return sentence
-// }
